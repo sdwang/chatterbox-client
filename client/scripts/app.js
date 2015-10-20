48317d24ce7ef018f9c3e8040ee899555832bf07
sdwang <siwang22@gmail.com>
2015-10-19 17:37:55 -0700

    (add) methods to app including addMessage and findRooms

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,19 +1,6 @@
 // YOUR CODE HERE:
 //https://api.parse.com/1/classes/chatterbox
 
-// var app = {};
-
-// app.url = "https://api.parse.com/1/classes/chatterbox";
-// app.type = 'GET';
-// app.success = function(data) {
-//   console.log(data);
-
-//   $('#chats').append(data);
-// };
-
-
-// $.ajax(app);
-
 var app = {
   url: "https://api.parse.com/1/classes/chatterbox",
   server: "https://api.parse.com/1/classes/chatterbox",
@@ -24,8 +11,30 @@ var app = {
 
 };
 
+
+
 app.init = function() {
   //Calls other methods to startup the app
+  //fetch
+  //loop through chats
+    //Call addMessage
+
+// var message = {
+//   username: 'shawndrost',
+//   text: 'trololo',
+//   roomname: '4chan'
+// };
+
+  $(document).ready(function(){
+    $(".submit").on("click", function(){
+      var chat = {};
+      chat.text = $('#message input').val();
+      chat.username = window.location.search.slice(10);
+      chat.roomname = $('.room select option').val();
+      app.send(chat);
+      app.addMessage(chat);
+    });
+  });
 };
 
 app.fetch = function() {
@@ -36,13 +45,54 @@ app.fetch = function() {
   $.ajax(app);
 };
 
-app.addMessage = function() {
-  for(var i = 0; app.chats.results.length; i++) {
-    var message = '<div>' + app.chats.results[i].username + ': ' + app.chats.results[i].text + '</div>';
-    $('#chats').append(message);
-  }
+app.addMessage = function(message) {
+  var chat = '<div>' + message.username + ': ' + message.text + '</div>';
+  $('#chats').prepend(chat);
 };
 
 app.clearMessages = function() {
   $('#chats').empty();
 };
+
+app.addRoom = function(roomname){
+  var room = '<option value=' + roomname + ">" + roomname + "</option>";
+  $('select').append(room);
+};
+
+app.send = function(message) {
+  app.type = "POST";
+  app.data = JSON.stringify(message);
+  app.success = function(data){
+    console.log('Message sent!');
+  };
+
+  app.error = function(data){
+    console.log("Failed to send message");
+  };
+  $.ajax(app);
+};
+
+app.findRooms = function(){
+  var obj = {Lobby:"Lobby"};
+  app.fetch();
+  for (var i = 0; i < app.chats.results.length; i++) {
+    if(app.chats.results[i].roomname === "Lobby") {
+      debugger;
+    }
+    if(obj[app.chats.results[i].roomname] === undefined){
+      obj[app.chats.results[i].roomname] = app.chats.results[i].roomname;
+    }
+  }
+  for(var key in obj){
+    app.addRoom(obj[key]);
+  }
+};
+
+
+
+
+
+
+app.init();
+
+
