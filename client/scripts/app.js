03f7d887fd5b50ad8a85a55838a9a2bb81910ee7
sdwang <siwang22@gmail.com>
2015-10-19 20:44:17 -0700

    (add) findRoom functionality. Fix asynchronous data retrieval problem

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,47 +1,63 @@
 // YOUR CODE HERE:
 //https://api.parse.com/1/classes/chatterbox
 
+
 var app = {
   url: "https://api.parse.com/1/classes/chatterbox",
   server: "https://api.parse.com/1/classes/chatterbox",
   type: undefined,
   success: undefined,
-
-  chats: undefined
-
+  chats: {}
 };
 
 
-
 app.init = function() {
-  //Calls other methods to startup the app
-  //fetch
-  //loop through chats
-    //Call addMessage
-
 // var message = {
 //   username: 'shawndrost',
 //   text: 'trololo',
 //   roomname: '4chan'
 // };
+$(document).ready(function(){
+  
+  $(".submit").on("click", function(){
+    
+    if($('.newRoom')[0].selected) {
+      var name = $('#message input').val();
+      app.addRoom(name);
 
-  $(document).ready(function(){
-    $(".submit").on("click", function(){
-      var chat = {};
-      chat.text = $('#message input').val();
-      chat.username = window.location.search.slice(10);
-      chat.roomname = $('.room select option').val();
-      app.send(chat);
-      app.addMessage(chat);
-    });
+    }
+
+
+    var chat = {};
+    chat.text = $('#message input').val();
+    chat.username = window.location.search.slice(10);
+    chat.roomname = $('.room select option').val();
+    app.send(chat);
+    app.addMessage(chat);
   });
+
+  $(".newRoom").on('click', function(){
+    $('#message input').attr('placeholder', 'Create new room');
+    app.clearMessages();
+
+  });
+});
+  
+  app.fetch();
+  
+
 };
+
 
 app.fetch = function() {
   app.type = 'GET';
   app.success = function(data) {
     app.chats = data;
+    app.findRooms();
   };
+  app.error = function(data) {
+    console.error('Failed to fetch messages');
+  }
   $.ajax(app);
 };
 
@@ -72,13 +88,11 @@ app.send = function(message) {
   $.ajax(app);
 };
 
+
+//app.fetch();
 app.findRooms = function(){
-  var obj = {Lobby:"Lobby"};
-  app.fetch();
-  for (var i = 0; i < app.chats.results.length; i++) {
-    if(app.chats.results[i].roomname === "Lobby") {
-      debugger;
-    }
+  var obj = {Lobby: "Lobby"};
+  for (var i = 0; i < app.chats['results'].length; i++) {
     if(obj[app.chats.results[i].roomname] === undefined){
       obj[app.chats.results[i].roomname] = app.chats.results[i].roomname;
     }
@@ -88,11 +102,25 @@ app.findRooms = function(){
   }
 };
 
+//Call before:
+//  app.fetch();
+app.showMessages = function() {
 
-
-
-
+  var room;
+  $(".room select").children().each(function(index, element){
+    if(element.selected === true){
+      room = element.value;
+    }
+  });
+  app.clearMessages();
+  for(var i = 0; i < app.chats.results.length; i++) {
+    if(app.chats.results[i].roomname === room) {
+      app.addMessage(app.chats.results[i]);
+    }
+  }
+};
 
 app.init();
+
 
 
